aac524f8917d160145871e04a24e01e68ab5dea1
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2021-11-05 21:13:18 -0200

    Creating Drawing Helper

---
+++ UzunTec.WinUI.Controls/Helpers/DrawingHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using UzunTec.WinUI.Controls.Interfaces;
+
+namespace UzunTec.WinUI.Controls.Helpers
+{
+    internal static class DrawingHelper
+    {
+        private const int LINE_BOTTOM_HEIGHT = 1;
+        private const int FOCUSED_LINE_BOTTOM_HEIGHT = 2;
+        private const int LINE_BOTTOM_PADDING = 1;
+
+        private static readonly ThemeSchemeManager themeManager = ThemeSchemeManager.Instance;
+
+        internal static void FillBackground(this Graphics g, IThemeControlWithBackground ctrl)
+        {
+
+            Brush backgroundBrush = ctrl.Enabled ?
+                            (ctrl.Focused || ctrl.MouseHovered) ? themeManager.GetFocusedBackgroundBrush(ctrl)
+                            : themeManager.GetBackgroundBrush(ctrl)
+                            : themeManager.GetDisabledBackgroundBrush(ctrl);
+
+            RectangleF bgRect = ctrl.ClientRectangle;
+            bgRect.Height -= LINE_BOTTOM_PADDING;
+            g.FillRectangle(backgroundBrush, bgRect);
+        }
+
+        internal static void DrawBottomLine(this Graphics g, IThemeControl ctrl)
+        {
+            Brush lineBrush = ctrl.Focused ? themeManager.GetHighlightBrush(ctrl) : themeManager.GetTextBrush(ctrl);
+            g.FillRectangle(lineBrush, GetBottomLineRect(ctrl));
+        }
+
+        internal static RectangleF GetBottomLineRect(this IThemeControl ctrl)
+        {
+            float lineHeight = ctrl.Focused ? FOCUSED_LINE_BOTTOM_HEIGHT : LINE_BOTTOM_HEIGHT;
+            float lineY = ctrl.ClientRectangle.Bottom - lineHeight - (ctrl.Focused ? 0 : LINE_BOTTOM_PADDING);
+            return new RectangleF(0, lineY, ctrl.ClientRectangle.Width, lineHeight);
+        }
+
+        internal static void DrawHint(this Graphics g, IThemeControlWithHint ctrl)
+        {
+            Brush hintBrush = themeManager.GetHintBrush(ctrl);
+            RectangleF hintRect = GetHintRect(ctrl, g);
+            g.DrawString(ctrl.PlaceholderHintText, ctrl.HintFont, hintBrush, hintRect);
+        }
+
+        internal static RectangleF GetHintRect(this IThemeControlWithHint ctrl, Graphics g)
+        {
+            SizeF hintSize = g.MeasureString(ctrl.PlaceholderHintText, ctrl.HintFont, ctrl.ClientRectangle.Width);
+            return new RectangleF(ctrl.InternalPadding.Left, ctrl.InternalPadding.Top, hintSize.Width, hintSize.Height);
+        }
+    }
+}
--- UzunTec.WinUI.Controls/ThemeSchemeManager.cs
+++ UzunTec.WinUI.Controls/ThemeSchemeManager.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UzunTec.WinUI.Controls.Helpers;
+using UzunTec.WinUI.Controls.Interfaces;
 
 namespace UzunTec.WinUI.Controls
 {
@@ -21,5 +25,36 @@ namespace UzunTec.WinUI.Controls
             return this.themeScheme;
         }
 
+        internal Brush GetFocusedBackgroundBrush(IThemeControlWithBackground ctrl)
+        {
+            return new LinearGradientBrush(ctrl.ClientRectangle, ctrl.FocusedBackgroundColorDark, ctrl.FocusedBackgroundColorLight, LinearGradientMode.Vertical);
+        }
+
+        internal Brush GetBackgroundBrush(IThemeControlWithBackground ctrl)
+        {
+            return new LinearGradientBrush(ctrl.ClientRectangle, ctrl.BackgroundColorDark, ctrl.BackgroundColorLight, LinearGradientMode.Vertical);
+        }
+
+        internal Brush GetDisabledBackgroundBrush(IThemeControlWithBackground ctrl)
+        {
+            return new LinearGradientBrush(ctrl.ClientRectangle, ctrl.DisabledBackgroundColorDark, ctrl.DisabledBackgroundColorLight, LinearGradientMode.Vertical);
+        }
+
+        internal Brush GetHighlightBrush(IThemeControl ctrl)
+        {
+            return new SolidBrush(ctrl.HighlightColor);
+        }
+
+        internal Brush GetTextBrush(IThemeControl ctrl)
+        {
+            return ctrl.Enabled ? new SolidBrush(ctrl.TextColor) : new SolidBrush(ctrl.DisabledTextColor);
+        }
+
+        internal Brush GetHintBrush(IThemeControlWithHint ctrl)
+        {
+            return ctrl.Enabled? 
+                ctrl.Focused ? this.GetHighlightBrush(ctrl) : new SolidBrush(ctrl.HintColor)
+                : new SolidBrush(ctrl.DisabledHintColor);
+        }
     }
 }
