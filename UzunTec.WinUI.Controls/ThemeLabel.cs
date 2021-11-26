dc8cb9f03a0029fec286cdc7e7cdfd029384cc83
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2021-11-26 20:09:32 -0200

    Adding Theme Label

---
+++ UzunTec.WinUI.Controls/ThemeLabel.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using UzunTec.WinUI.Controls.Helpers;
+using UzunTec.WinUI.Controls.Interfaces;
+using UzunTec.WinUI.Utils;
+
+namespace UzunTec.WinUI.Controls
+{
+    public class ThemeLabel : Label, IThemeControlWithBackground
+    {
+        [Browsable(false), ReadOnly(true)]
+        public new Color BackColor { get => this.BackgroundColorDark; set => this.BackgroundColorDark = value; }
+
+        [Browsable(false), ReadOnly(true)]
+        public new Color ForeColor { get => this.TextColor; set => this.TextColor = value; }
+
+        [Browsable(false)]
+        public ThemeScheme ThemeScheme => ThemeSchemeManager.Instance.GetTheme();
+
+        [Browsable(false)]
+        public bool MouseHovered { get; private set; }
+
+        #region Theme Properties
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Control")]
+        public Color BackgroundColorDark { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Control")]
+        public Color BackgroundColorLight { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Control")]
+        public Color DisabledBackgroundColorDark { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Control")]
+        public Color DisabledBackgroundColorLight { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Control")]
+        public Color FocusedBackgroundColorDark { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Control")]
+        public Color FocusedBackgroundColorLight { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Red")]
+        public Color HighlightColor { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Gray")]
+        public Color DisabledTextColor { get; set; }
+
+        [Category("Theme"), DefaultValue(typeof(Color), "Black")]
+        public Color TextColor { get; set; }
+
+        #endregion
+
+        [Category("Z-Custom"), DefaultValue(typeof(Padding), "1; 1; 1; 1;")]
+        public Padding InternalPadding { get => this._internalPadding; set { this._internalPadding = value; this.Invalidate(); } }
+        private Padding _internalPadding;
+
+        [Category("Z-Custom"), DefaultValue(false)]
+        public bool ShowBackground { get => this._showBackground; set { this._showBackground = value; this.Invalidate(); } }
+        private bool _showBackground;
+
+        public ThemeLabel()
+        {
+            this._internalPadding = new Padding(1);
+            this._showBackground = false;
+
+            // Theme
+            this.Font = this.ThemeScheme.ControlTextFont;
+            this.TextColor = this.ThemeScheme.ControlTextColor;
+            this.DisabledTextColor = this.ThemeScheme.DisabledControlTextColor;
+            this.HighlightColor = this.ThemeScheme.ControlHighlightColor;
+            this.BackgroundColorDark = this.ThemeScheme.ControlBackgroundColorDark;
+            this.BackgroundColorLight = this.ThemeScheme.ControlBackgroundColorLight;
+            this.FocusedBackgroundColorDark = this.ThemeScheme.ControlBackgroundColorLight;
+            this.FocusedBackgroundColorLight = this.ThemeScheme.ControlBackgroundColorLight;
+            this.DisabledBackgroundColorDark = this.ThemeScheme.DisabledControlBackgroundColorDark;
+            this.DisabledBackgroundColorLight = this.ThemeScheme.DisabledControlBackgroundColorLight;
+        }
+
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw, true);
+            MouseEnter += (sender, args) => { MouseHovered = true; this.Invalidate(); };
+            MouseLeave += (sender, args) => { MouseHovered = false; this.Invalidate(); };
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            Graphics g = pevent.Graphics;
+            g.Clear(Parent.BackColor);
+
+            if (this._showBackground)
+            {
+                g.FillBackground(this, false);
+            }
+            RectangleF textRect = ClientRectangle.ToRectF().ApplyPadding(this._internalPadding);
+            Brush textBrush = ThemeSchemeManager.Instance.GetTextBrush(this);
+            g.DrawText(this.Text, this.Font, textBrush, textRect, this.TextAlign);
+        }
+
+        public override Size GetPreferredSize(Size proposedSize)
+        {
+            Size s = base.GetPreferredSize(proposedSize);
+            return new Size(s.Width + this._internalPadding.Horizontal, s.Height + this._internalPadding.Vertical);
+        }
+    }
+}
