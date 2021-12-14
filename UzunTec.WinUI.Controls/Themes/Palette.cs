ea760da96149f0f928b2f716f596ec8143048d47
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2021-12-13 23:13:57 -0200

    New Palette Functions

--- UzunTec.WinUI.Controls/Themes/Palette.cs
+++ UzunTec.WinUI.Controls/Themes/Palette.cs
@@ -1,54 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using UzunTec.Utils.Common;
 
 namespace UzunTec.WinUI.Controls.Themes
-
 {
     public class Palette
     {
-        private readonly SortedList<ColorVariant, Lazy<Brush>> brushes;
+        private readonly SortedList<ColorVariant, Color[]> colors;
+        private readonly SortedList<ColorVariant, Brush> brushes;
 
         public Palette()
         {
-            this.brushes = new SortedList<ColorVariant, Lazy<Brush>>
-            {
-                {ColorVariant.Primary, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, PrimaryColorDark, PrimaryColorLight, LinearGradientMode.Vertical)) },
-                {ColorVariant.Secondary, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, SecondaryColorDark, SecondaryColorLight, LinearGradientMode.Vertical))},
-                {ColorVariant.Info, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, SuccessColorDark, SuccessColorLight, LinearGradientMode.Vertical))},
-                {ColorVariant.Success, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, PrimaryColorDark, ColorLight, LinearGradientMode.Vertical))},
-                {ColorVariant.Warning, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, PrimaryColorDark, PrimaryColorLight, LinearGradientMode.Vertical))},
-                {ColorVariant.Danger, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, PrimaryColorDark, PrimaryColorLight, LinearGradientMode.Vertical))},
-                {ColorVariant.Light, new Lazy<Brush>(() =>  new LinearGradientBrush(ThemeConstants.DefaultControlRect, PrimaryColorDark, PrimaryColorLight, LinearGradientMode.Vertical))},
-            };
+            this.colors = new SortedList<ColorVariant, Color[]>();
+            this.brushes = new SortedList<ColorVariant, Brush>();
         }
 
         public Color GetColor(ColorVariant variant, bool dark = true)
         {
-            return Color.Red;   // TODO:
+            if (this.colors.TryGetValue(variant, out Color[] colorPair))
+            {
+                if (colorPair != null && colorPair.Length > 0)
+                {
+                    return (colorPair.Length == 1) ? colorPair[0]
+                        : (dark) ? colorPair[0] : colorPair[1];
+                }
+            }
+            return default;
         }
 
-
-        private static Brush GetSecondaryBrush()
+        public Brush GetBrush(ColorVariant variant)
         {
-            throw new NotImplementedException();
+            if (!this.brushes.TryGetValue(variant, out Brush output))
+            {
+                if (this.colors.TryGetValue(variant, out Color[] colorPair))
+                {
+                    if (colorPair != null && colorPair.Length > 0)
+                    {
+                        output = (colorPair.Length == 1) ? new SolidBrush(colorPair[0])
+                                    : (Brush)new LinearGradientBrush(ThemeConstants.DefaultControlRect, colorPair[0], colorPair[1], LinearGradientMode.Vertical);
+
+                        this.brushes.Add(variant, output);
+                    }
+                }
+            }
+            return output;
         }
 
-        public Color PrimaryColorDark { get; set; }
-        public Color PrimaryColorLight { get; set; }
-        public Color SecondaryColorDark { get; set; }
-        public Color SecondaryColorLight { get; set; }
-        public Color SuccessColorDark { get; set; }
-        public Color SuccessColorLight { get; set; }
-        public Color InfoColorDark { get; set; }
-        public Color InfoColorLight { get; set; }
-        public Color WarningColorDark { get; set; }
-        public Color WarningColorLight { get; set; }
-        public Color DangerColorDark { get; set; }
-        public Color DangerColorLight { get; set; }
-        public Color LightColor { get; set; }
-
-
+        public void SetColor(ColorVariant variant, params Color[] colors)
+        {
+            this.colors.AddOrUpdate(variant, colors);
+        }
     }
 }
--- UzunTec.WinUI.Controls/Themes/Palettes.cs
+++ UzunTec.WinUI.Controls/Themes/Palettes.cs
@@ -4,38 +4,28 @@ namespace UzunTec.WinUI.Controls.Themes
 {
     public static class Palettes
     {
-        public static Palette Flat = new Palette
-        {
-            PrimaryColorDark = Color.FromArgb(41, 128, 185),
-            PrimaryColorLight = Color.FromArgb(52, 152, 219),
-            SecondaryColorDark = Color.FromArgb(189, 195, 199),
-            SecondaryColorLight = Color.FromArgb(236, 240, 241),
-            InfoColorDark = Color.FromArgb(41, 128, 185),
-            InfoColorLight = Color.FromArgb(52, 152, 219),
-            SuccessColorDark = Color.FromArgb(39, 174, 96),
-            SuccessColorLight = Color.FromArgb(46, 204, 113),
-            WarningColorDark = Color.FromArgb(230, 126, 34),
-            WarningColorLight = Color.FromArgb(241, 196, 15),
-            DangerColorDark = Color.FromArgb(192, 57, 43),
-            DangerColorLight = Color.FromArgb(192, 57, 43),
-            LightColor = Color.FromArgb(236, 240, 241)
-        };
+        public static Palette Flat = new Palette();
+        public static Palette Aussie = new Palette();
 
-        public static Palette Aussie = new Palette
+        static Palettes()
         {
-            PrimaryColorDark = Color.FromArgb(19, 15, 64),
-            PrimaryColorLight = Color.FromArgb(48, 51, 107),
-            SecondaryColorDark = Color.FromArgb(199, 236, 238),
-            SecondaryColorLight = Color.FromArgb(223, 249, 251),
-            InfoColorDark = Color.FromArgb(34, 166, 179),
-            InfoColorLight = Color.FromArgb(126, 214, 223),
-            SuccessColorDark = Color.FromArgb(106, 176, 76),
-            SuccessColorLight = Color.FromArgb(186, 220, 88),
-            WarningColorDark = Color.FromArgb(249, 202, 36),
-            WarningColorLight = Color.FromArgb(246, 229, 141),
-            DangerColorDark = Color.FromArgb(235, 77, 75),
-            DangerColorLight = Color.FromArgb(255, 121, 121),
-            LightColor = Color.FromArgb(236, 240, 241),
-        };
+            Flat.SetColor(ColorVariant.Primary, Color.FromArgb(41, 128, 185), Color.FromArgb(52, 152, 219));
+            Flat.SetColor(ColorVariant.Secondary, Color.FromArgb(189, 195, 199), Color.FromArgb(236, 240, 241));
+            Flat.SetColor(ColorVariant.Info, Color.FromArgb(41, 128, 185), Color.FromArgb(52, 152, 219));
+            Flat.SetColor(ColorVariant.Success, Color.FromArgb(39, 174, 96), Color.FromArgb(46, 204, 113));
+            Flat.SetColor(ColorVariant.Warning, Color.FromArgb(230, 126, 34), Color.FromArgb(241, 196, 15));
+            Flat.SetColor(ColorVariant.Danger, Color.FromArgb(192, 57, 43), Color.FromArgb(192, 57, 43));
+            Flat.SetColor(ColorVariant.Light, Color.FromArgb(236, 240, 241));
+            Flat.SetColor(ColorVariant.Dark, Color.FromArgb(44, 62, 80));
+
+            Aussie.SetColor(ColorVariant.Primary, Color.FromArgb(19, 15, 64), Color.FromArgb(48, 51, 107));
+            Aussie.SetColor(ColorVariant.Secondary, Color.FromArgb(199, 236, 238), Color.FromArgb(223, 249, 251));
+            Aussie.SetColor(ColorVariant.Info, Color.FromArgb(34, 166, 179), Color.FromArgb(126, 214, 223));
+            Aussie.SetColor(ColorVariant.Success, Color.FromArgb(106, 176, 76), Color.FromArgb(186, 220, 88));
+            Aussie.SetColor(ColorVariant.Warning, Color.FromArgb(249, 202, 36), Color.FromArgb(246, 229, 141));
+            Aussie.SetColor(ColorVariant.Danger, Color.FromArgb(235, 77, 75), Color.FromArgb(255, 121, 121));
+            Aussie.SetColor(ColorVariant.Light, Color.FromArgb(236, 240, 241));
+            Aussie.SetColor(ColorVariant.Dark, Color.FromArgb(0, 0, 0));
+        }
     }
 }
