3c6dc06dc6a19563408ae8ac0c7d298da23dd3fa
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2021-12-28 18:31:43 -0200

    Adding More Non client Events

--- UzunTec.WinUI.Controls/FormWithNc.cs
+++ UzunTec.WinUI.Controls/FormWithNc.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -9,85 +9,19 @@ namespace UzunTec.WinUI.Controls
 {
     public class FormWithNc : Form
     {
-        /*
-         * Constants
-         */
-        //Paramaters to EnableMenuItem Win32 function
-        private const int SC_CLOSE = 0xF060; //The Close Box identifier
-        private const int MF_ENABLED = 0x0;  //Enabled Value
-        private const int MF_DISABLED = 0x2; //Disabled Value
-
-        //Windows Messages
-        private const int WM_NCPAINT = 0x85;//Paint non client area message
-        private const int WM_PAINT = 0xF;//Paint client area message
-        private const int WM_SIZE = 0x5;//Resize the form message
-        private const int WM_IME_NOTIFY = 0x282;//Notify IME Window message
-        private const int WM_SETFOCUS = 0x0007;//Form.Activate message
-        private const int WM_SYSCOMMAND = 0x112; //SysCommand message
-        private const int WM_SIZING = 0x214; //Resize Message
-        private const int WM_NCLBUTTONDOWN = 0xA1; //Left Mouse Button on Non-Client Area is Down
-        private const int WM_NCACTIVATE = 0x86; //Message sent to the window when it's activated or deactivated
-        private const int WM_NCCALCSIZE = 0x0083;
-
-        //WM_SIZING WParams that stands for Hit Tests in the direction the form is resizing
-        private const int HHT_ONHEADER = 0x0002;
-        private const int HT_TOPLEFT = 0XD;
-        private const int HT_TOP = 0XC;
-        private const int HT_TOPRIGHT = 0XE;
-        private const int HT_RIGHT = 0XB;
-        private const int HT_BOTTOMRIGHT = 0X11;
-        private const int HT_BOTTOM = 0XF;
-        private const int HT_BOTTOMLEFT = 0X10;
-        private const int HT_LEFT = 0XA;
-
-        //WM_SYSCOMMAND WParams that stands for which operation is beeing done
-        private const int SC_DRAGMOVE = 0xF012; //SysCommand Dragmove parameter
-        private const int SC_MOVE = 0xF010; //SysCommand Move with keyboard command
 
         private Graphics ncGrapichs = null;
         private IntPtr wndHdc;
 
-       
-        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
-        public extern static void ReleaseCapture();
-        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
-        public extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
-
-        public const int CB_SETCUEBANNER = 0x1703;
-        public const int EM_SETCUEBANNER = 0x1501;
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
-
-        [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
-        public static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
-
-        /// <summary>
-        /// Occurs when the frame area (including Title Bar, excluding the client area) is redrawn."
-        /// </summary>
-        [Description("Occurs when The frame area (including Title Bar, excluding the client area) needs repainting."), Category("Appearance")]
         public event PaintEventHandler NcPaint;
         public event EventHandler NcAreaChanged;
-
-
-        //Get Desktop Window Handle
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetDesktopWindow();
-
-
-        //Find any Window in the OS. We will look for the parent of where the desktop is (ProgMan)
-        [DllImport("User32.dll")]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        //Get the device component of the window to allow drawing on the title bar and frame
-        [DllImport("User32.dll")]
-        public static extern IntPtr GetWindowDC(IntPtr hWnd);
-
-        //Releases the Device Component after it's been used
-        [DllImport("User32.dll")]
-        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        public event MouseEventHandler NcMouseMove;
+        public event MouseEventHandler NcMouseLeave;
+        public event MouseEventHandler NcMouseDown;
+        public event MouseEventHandler NcMouseUp;
 
         public Padding NonClientArea { get; private set; }
-        public object RectClient2 { get; private set; }
+        public Rectangle RectClient2 { get; private set; }
         public Rectangle RectNewWindow { get; private set; }
         public Rectangle RectOldWindow { get; private set; }
         public Rectangle RectClient { get; private set; }
@@ -111,7 +45,7 @@ namespace UzunTec.WinUI.Controls
         private void InvalidateAll()
         {
             this.Invalidate();
-            SendMessage(this.Handle, WM_NCPAINT, 1, 0);
+            Win32ApiFunction.SendMessage(this.Handle, Win32ApiConstants.WM_NCPAINT, 1, 0); ;
         }
 
         protected override void OnCreateControl()
@@ -123,8 +57,14 @@ namespace UzunTec.WinUI.Controls
         protected override void OnHandleCreated(EventArgs e)
         {
             base.OnHandleCreated(e);
-            SetWindowTheme(this.Handle, string.Empty, string.Empty);
+            Win32ApiFunction.SetWindowTheme(this.Handle, string.Empty, string.Empty);
         }
+
+        private readonly List<int> NcMouseWindowsMessages = new List<int> {
+            Win32ApiConstants.WM_NCMOUSEMOVE,Win32ApiConstants.WM_NCMOUSELEAVE,
+            Win32ApiConstants.WM_NCLBUTTONDOWN, Win32ApiConstants.WM_NCLBUTTONUP, Win32ApiConstants.WM_NCLBUTTONDBLCLK,
+            Win32ApiConstants.WM_NCRBUTTONDOWN, Win32ApiConstants.WM_NCRBUTTONUP, Win32ApiConstants.WM_NCRBUTTONDBLCLK,
+            Win32ApiConstants.WM_NCMBUTTONDOWN, Win32ApiConstants.WM_NCMBUTTONUP, Win32ApiConstants.WM_NCMBUTTONDBLCLK };
 
 
         protected override void WndProc(ref Message m)
@@ -132,66 +72,114 @@ namespace UzunTec.WinUI.Controls
             base.WndProc(ref m);
 
             // Handles painting of the Non Client Area
-            if (m.Msg == WM_NCPAINT || m.Msg == WM_IME_NOTIFY || m.Msg == WM_SIZE || m.Msg == WM_NCACTIVATE)
+            if (m.Msg == Win32ApiConstants.WM_NCPAINT || m.Msg == Win32ApiConstants.WM_IME_NOTIFY || m.Msg == Win32ApiConstants.WM_SIZE || m.Msg == Win32ApiConstants.WM_NCACTIVATE)
             {
-                // To avoid unnecessary graphics recreation and thus improving performance
-                if (DesignMode || ncGrapichs == null || m.Msg == WM_SIZE)
-                {
-                    ReleaseDC(this.Handle, wndHdc);
-                    wndHdc = GetWindowDC(this.Handle);
-                    ncGrapichs = Graphics.FromHdc(wndHdc);
-
-                    if (!DesignMode)
-                    {
-                        Rectangle clientRecToScreen = new Rectangle(this.PointToScreen(new Point(this.ClientRectangle.X, this.ClientRectangle.Y)), new Size(this.ClientRectangle.Width, this.ClientRectangle.Height));
-                        Rectangle clientRectangle = new Rectangle(clientRecToScreen.X - this.Location.X, clientRecToScreen.Y - this.Location.Y, clientRecToScreen.Width, clientRecToScreen.Height);
-
-                        ncGrapichs.ExcludeClip(clientRectangle);
-                    }
-                }
-
-                RectangleF recF = ncGrapichs.VisibleClipBounds;
-
-                if (!this.Disposing)
-                {
-                    PaintEventArgs ncPaintEventArgs = new PaintEventArgs(ncGrapichs, new Rectangle((int)recF.X, (int)recF.Y, (int)recF.Width, (int)recF.Height));
-                    OnNcPaint(ncPaintEventArgs);
-                }
-                this.Refresh();
-
+                this.NcPaintMsgHandler(ref m);
             }
-            else if (m.Msg == WM_NCCALCSIZE)
+            else if (m.Msg == Win32ApiConstants.WM_NCCALCSIZE)
             {
-                if (m.WParam != IntPtr.Zero)
-                {
-                    NCCALCSIZE_PARAMS rcsize = (NCCALCSIZE_PARAMS)Marshal.PtrToStructure(m.LParam, typeof(NCCALCSIZE_PARAMS));
-                    AdjustClientRect(ref rcsize.rcNewWindow);
-                    Marshal.StructureToPtr(rcsize, m.LParam, false);
-
-                    this.RectNewWindow = rcsize.rcNewWindow.ToRectangle();
-                    this.RectOldWindow = rcsize.rcOldWindow.ToRectangle();
-                    this.RectClient = rcsize.rcClient.ToRectangle();
-
-                    this.CheckNCArea (new Padding(rcsize.rcClient.Left - rcsize.rcOldWindow.Left,
-                                                    rcsize.rcClient.Top - rcsize.rcOldWindow.Top,
-                                                    rcsize.rcOldWindow.Right - rcsize.rcClient.Right,
-                                                    rcsize.rcOldWindow.Bottom - rcsize.rcClient.Bottom));
-
-                }
-                else
-                {
-                    RECT rcsize = (RECT)Marshal.PtrToStructure(m.LParam, typeof(RECT));
-                    AdjustClientRect(ref rcsize);
-                    Marshal.StructureToPtr(rcsize, m.LParam, false);
-                    this.RectClient2 = rcsize.ToRectangle();
-
-                }
-                m.Result = new IntPtr(1);
-                return;
+                this.NcCalcSizeMsgHandler(ref m);
+            }
+            else if (NcMouseWindowsMessages.Contains(m.Msg))
+            {
+                this.NcMouseMsgHandler(ref m);
             }
         }
 
-   
+
+        private void NcCalcSizeMsgHandler(ref Message m)
+        {
+            if (m.WParam != IntPtr.Zero)
+            {
+                NCCALCSIZE_PARAMS rcsize = (NCCALCSIZE_PARAMS)Marshal.PtrToStructure(m.LParam, typeof(NCCALCSIZE_PARAMS));
+                AdjustClientRect(ref rcsize.rcNewWindow);
+                Marshal.StructureToPtr(rcsize, m.LParam, false);
+
+                this.RectNewWindow = rcsize.rcNewWindow.ToRectangle();
+                this.RectOldWindow = rcsize.rcOldWindow.ToRectangle();
+                this.RectClient = rcsize.rcClient.ToRectangle();
+
+                this.CheckNCArea(new Padding(rcsize.rcClient.Left - rcsize.rcOldWindow.Left,
+                                                rcsize.rcClient.Top - rcsize.rcOldWindow.Top,
+                                                rcsize.rcOldWindow.Right - rcsize.rcClient.Right,
+                                                rcsize.rcOldWindow.Bottom - rcsize.rcClient.Bottom));
+
+            }
+            else
+            {
+                RECT rcsize = (RECT)Marshal.PtrToStructure(m.LParam, typeof(RECT));
+                AdjustClientRect(ref rcsize);
+                Marshal.StructureToPtr(rcsize, m.LParam, false);
+                this.RectClient2 = rcsize.ToRectangle();
+
+            }
+            m.Result = new IntPtr(1);
+            return;
+        }
+
+        private void NcPaintMsgHandler(ref Message m)
+        {
+            // To avoid unnecessary graphics recreation and thus improving performance
+            if (DesignMode || ncGrapichs == null || m.Msg == Win32ApiConstants.WM_SIZE)
+            {
+                Win32ApiFunction.ReleaseDC(this.Handle, wndHdc);
+                wndHdc = Win32ApiFunction.GetWindowDC(this.Handle);
+                ncGrapichs = Graphics.FromHdc(wndHdc);
+
+                if (!DesignMode)
+                {
+                    Rectangle clientRecToScreen = new Rectangle(this.PointToScreen(new Point(this.ClientRectangle.X, this.ClientRectangle.Y)), new Size(this.ClientRectangle.Width, this.ClientRectangle.Height));
+                    Rectangle clientRectangle = new Rectangle(clientRecToScreen.X - this.Location.X, clientRecToScreen.Y - this.Location.Y, clientRecToScreen.Width, clientRecToScreen.Height);
+
+                    ncGrapichs.ExcludeClip(clientRectangle);
+                }
+            }
+
+            RectangleF recF = ncGrapichs.VisibleClipBounds;
+
+            if (!this.Disposing)
+            {
+                PaintEventArgs ncPaintEventArgs = new PaintEventArgs(ncGrapichs, new Rectangle((int)recF.X, (int)recF.Y, (int)recF.Width, (int)recF.Height));
+                OnNcPaint(ncPaintEventArgs);
+            }
+            this.Refresh();
+        }
+
+        private void NcMouseMsgHandler(ref Message m)
+        {
+            Point ptScreen = Cursor.Position;
+            Point ptClient = PointToClient(ptScreen);
+
+            int x = ptClient.X + this.NonClientArea.Left;
+            int y = ptClient.Y + this.NonClientArea.Top;
+            MouseEventArgs e = new MouseEventArgs(Control.MouseButtons, 0, x, y, 0);
+
+            switch (m.Msg)
+            {
+                case Win32ApiConstants.WM_NCMOUSEMOVE:
+                    this.OnNcMouseMove(e);
+                    break;
+
+                case Win32ApiConstants.WM_NCMOUSELEAVE:
+                    this.OnNcMouseLeave(e);
+                    break;
+
+                case Win32ApiConstants.WM_NCLBUTTONDOWN:
+                case Win32ApiConstants.WM_NCRBUTTONDOWN:
+                case Win32ApiConstants.WM_NCMBUTTONDOWN:
+                    this.OnNcMouseDown(e);
+                    break;
+
+                case Win32ApiConstants.WM_NCLBUTTONUP:
+                case Win32ApiConstants.WM_NCRBUTTONUP:
+                case Win32ApiConstants.WM_NCMBUTTONUP:
+                    this.OnNcMouseUp(e);
+                    break;
+            }
+        }
+
+     
+
         private void AdjustClientRect(ref RECT rcClient)
         {
             rcClient.Left += this._nonClientAreaAdjust.Left;
@@ -220,17 +208,36 @@ namespace UzunTec.WinUI.Controls
             NcAreaChanged?.Invoke(this, e);
         }
 
-        [EditorBrowsable(EditorBrowsableState.Advanced)]
         protected virtual void OnNcPaint(PaintEventArgs e)
         {
             NcPaint?.Invoke(this, e);
+        }
+
+        protected virtual void OnNcMouseMove(MouseEventArgs e)
+        {
+            NcMouseMove?.Invoke(this, e);
+        }
+
+        protected virtual void OnNcMouseLeave(MouseEventArgs e)
+        {
+            NcMouseLeave?.Invoke(this, e);
+        }
+
+        protected virtual void OnNcMouseDown(MouseEventArgs e)
+        {
+            NcMouseDown?.Invoke(this, e);
+        }
+
+        protected virtual void OnNcMouseUp(MouseEventArgs e)
+        {
+            NcMouseUp?.Invoke(this, e);
         }
 
         protected override void OnClosed(EventArgs e)
         {
             base.OnClosed(e);
             NcPaint = null;
-            ReleaseDC(this.Handle, wndHdc);
+            Win32ApiFunction.ReleaseDC(this.Handle, wndHdc);
         }
     }
 }
