5055f986ab093bf2b0feb1a8fb757b5a37eaf643
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2022-01-14 17:38:14 -0200

    ColorVariant Adjustments

--- UzunTec.WinUI.Controls/Themes/ColorVariant.cs
+++ UzunTec.WinUI.Controls/Themes/ColorVariant.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UzunTec.WinUI.Controls.Themes
+﻿namespace UzunTec.WinUI.Controls.Themes
 {
     public enum ColorVariant
     {
-        Dark,       // Black
-        Light,      // White
-        Primary,    // Blue / Purple
-        Secondary,  // Grey Variant
-        Success,    // Green
-        Info,       // Blue / Cyan
-        Warning,    // Yellow
-        Danger,     // Red
+        Dark = 'B',       // Black
+        Light = 'W',      // White
+        Primary = 'P',    // Blue / Purple
+        Secondary = 'G',  // Grey Variant
+        Success = 'S',    // Green
+        Info = 'I',       // Blue / Cyan
+        Warning = 'W',    // Yellow
+        Danger = 'D',     // Red
     }
 }
 
